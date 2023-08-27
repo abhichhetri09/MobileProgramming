a9c1ece72c9415415d05c76927e6605ca1bcd97d
Abhishek chhetri <abhichhetri@outlook.com>
2023-08-27 23:54:54 +0300

    Calculator with history done

--- App.js
+++ App.js
@@ -1,59 +1,107 @@
-import {useState} from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert, TextInput} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { useState } from "react";
+import { Button } from 'react-native-elements';
+import { StyleSheet, Text, View, TextInput, FlatList, ScrollView } from 'react-native';
 
 export default function App() {
- const [num1, setNum1] = useState('');
+  const [num1, setNum1] = useState('');
   const [num2, setNum2] = useState('');
   const [result, setResult] = useState('');
+  const [history, setHistory] = useState([]);
 
   const calculateSum = () => {
     const sum = parseFloat(num1) + parseFloat(num2);
     setResult(sum.toString());
+    setHistory(prevHistory => [...prevHistory, `${num1} + ${num2} = ${sum}`]);
   };
 
+ 
   const calculateSubtraction = () => {
     const subtraction = parseFloat(num1) - parseFloat(num2);
     setResult(subtraction.toString());
+    setHistory(prevHistory => [...prevHistory, `${num1} - ${num2} = ${subtraction}`]);
   };
-
-  return (
-     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      {result !== '' && <Text style={{ fontSize: 20, marginTop: 20 }}>Result: {result}</Text>}
-      <TextInput
-        style={{ borderWidth: 1, padding: 10, marginBottom: 10, width: 200 }}
-        
-        keyboardType="numeric"
-        value={num1}
-        onChangeText={setNum1}
-      />
-      <TextInput
-        style={{ borderWidth: 1, padding: 10, marginBottom: 10, width: 200 }}
-        
-        keyboardType="numeric"
-        value={num2}
-        onChangeText={setNum2}
-      />
-      <Button
-        title="+"
-        onPress={calculateSum}
-      />
-      <Button
-        title="-"
-        onPress={calculateSubtraction}
-      />
-      
+  const renderItem = ({ item }) => (
+    <View style={styles.historyItem}>
+      <Text>{item}</Text>
     </View>
   );
-    
+
+  return (
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        {result !== '' && <Text style={styles.resultText}>Result: {result}</Text>}
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter a number"
+            keyboardType="numeric"
+            value={num1}
+            onChangeText={setNum1}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Enter a number"
+            keyboardType="numeric"
+            value={num2}
+            onChangeText={setNum2}
+          />
+          <Button style={styles.button} title="Add +" onPress={calculateSum} />
+          <Button title="Subtract -" onPress={calculateSubtraction} />
+          <Text style={styles.historyText}>History</Text>
+        </View>
+        <FlatList
+          data={history}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  contentContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: '20%'
+  },
+  inputContainer: {
+    alignItems: 'center',
+    marginBottom: 10,
+    marginTop: 'auto',
+    marginBottom: 'auto',
+  },
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+    width: 200,
+  },
+  button:{
+    backgroundColor: 'skyblue',
+    
+    padding: 2,
+    borderRadius: 1,
+
+
+
+  },
+  resultText: {
+    fontSize: 20,
+    marginTop: 20,
+  },
+  historyItem: {
+   
+    paddingVertical: 10,
+  },
+  historyText: {
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 5,
+  }
 });
